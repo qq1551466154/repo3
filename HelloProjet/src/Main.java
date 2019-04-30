23c7763bb8471e21a81c3b1d5d85e952d555cda5
zhangsan <1551466154@qq.com>
2019-04-30 15:13:33 +0800

    修改一次s

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -5,6 +5,7 @@ public class Main {
     @Test
     public void testHelloWrold() {
         System.out.println(1);
+        System.out.println(2);
         System.out.println("HelloWorld!");
     }
 }
